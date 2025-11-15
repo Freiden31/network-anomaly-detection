9d9d5bdf23fa7ae720e18ec5d9a199ec1280fd46
Freiden31 <freidenduliente02@gmail.com>
2025-11-15 14:15:38 +0800

    Initial Commit

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -50,9 +50,8 @@ function Form({ route, method, onOtpRequest, onForgotPassword }) {
         formData.append("role", role);
         if (image) formData.append("profile_image", image);
 
-        res = await api.post(route, formData, {
-          headers: { "Content-Type": "multipart/form-data" },
-        });
+        res = await api.post(route, formData); // Let axios handle the Content-Type
+
         setMessage("Registration successful! Please check your email for OTP.");
         localStorage.setItem("email", email);
         if (typeof onOtpRequest === "function") {
